ee38d1c95365085e9c7a0036ae5d4e99f9eb8649
war99 <warshamalindi@gmail.com>
2025-12-31 21:16:01 +0530

    Update the appsetting and program.cs

--- Program.cs
+++ Program.cs
@@ -16,6 +16,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
+
 // Configure the HTTP request pipeline.
 
 app.UseSwagger();
